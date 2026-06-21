b1c943d1f4ae18f6f5faf68a88b31c1f5bbdd438
Captain-Bridge <1147871868@qq.com>
2026-06-21 21:03:30 +0800

    icon更新

--- source/marathon-lore/assets/app.js
+++ source/marathon-lore/assets/app.js
@@ -1755,14 +1755,7 @@ function entriesView() {
                         ${logCount > 0 ? `
                           <span class="entry-log-badge" aria-label="日志数量 ${logCount}/${logCount}">
                             <span class="entry-log-badge-icon">
-                              <svg viewBox="0 0 16 16" aria-hidden="true" focusable="false">
-                                <circle cx="8" cy="8" r="2.1"></circle>
-                                <path d="M8 1.5c1.4 0 2.6.5 3.6 1.4-1.8.2-3.1 1.2-3.9 2.8C7 4.6 5.8 3.8 4 3.5A5.7 5.7 0 0 1 8 1.5Z"></path>
-                                <path d="M13.6 4.1c.7 1.2.9 2.5.7 3.8-1.1-1.4-2.5-2-4.3-1.9.5-1.5.1-3-.9-4.5 1.8.2 3.2 1 4.5 2.6Z"></path>
-                                <path d="M12 13.2a6.2 6.2 0 0 1-3.7 1.2c1-1.5 1.1-3 .3-4.7 1.6.1 3-.7 4.2-2.1.3 2-.1 3.8-.8 5.6Z"></path>
-                                <path d="M3.8 13.1A6.4 6.4 0 0 1 2 7.7c1.2 1.2 2.6 1.7 4.3 1.4-.7 1.5-.5 3 .4 4.6-1.1-.1-2.1-.3-2.9-.6Z"></path>
-                                <path d="M2.5 4.3c.9-.8 1.9-1.3 3-1.6-.9 1.5-1 3-.2 4.7C3.7 7.2 2.3 8 1.1 9.4A6.3 6.3 0 0 1 2.5 4.3Z"></path>
-                              </svg>
+                              <span class="lore-mask-icon green" aria-hidden="true"></span>
                             </span>
                             <span class="entry-log-badge-count">${logCount}/${logCount}</span>
                           </span>
